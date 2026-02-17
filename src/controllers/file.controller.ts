eedc65db4a61bb0e8002219e295585112b5433a8
Harsh-Vdn25 <hars.vdn16@gmail.com>
2026-02-17 21:52:48 +0530

    getVersion and delVersion added

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { prisma } from "../config/prismaClient";
 import { findUserFile } from "../helpers/fileHelper";
 import {
   deleteAllService,
@@ -7,15 +6,20 @@ import {
   uploadService,
 } from "../services/file.service";
 import { storage } from "../storage/S3Storage";
+import { JwtPayload } from "jsonwebtoken";
+import { prisma } from "../config/prismaClient";
 
 export const uploadFile = async (req: Request, res: Response) => {
   const file = req.file;
   const userId = (req as any).userId;
   const isPrivate = req.body;
-  if (!file || typeof isPrivate === 'boolean') return res.status(400).json({ message: "Send all the required information." });
+  if (!file || typeof isPrivate === "boolean")
+    return res
+      .status(400)
+      .json({ message: "Send all the required information." });
 
   try {
-    const result = await uploadService(file, userId,isPrivate);
+    const result = await uploadService(file, userId, isPrivate);
     if (result === "INCOMPLETE_DETAILS") {
       return res
         .status(400)
@@ -106,6 +110,98 @@ export const deleteFile = async (req: Request, res: Response) => {
     }
 
     res.status(200).json({ message: "File deleted." });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getVersion = async (req: Request, res: Response) => {
+  const filename = req.params.filename;
+  const version = Number(req.params.version);
+  const userId = (req as any).userId;
+  if (!filename || typeof filename !== "string" || !version)
+    return res.status(400).json({ message: "Incomplete request." });
+  try {
+    const saved = await findUserFile(userId, filename);
+    if (!saved.success)
+      return res.status(404).json({ message: "File doesn't exist." });
+
+    const fileId = saved.savedFile?.id;
+    const fileVersion = await prisma.fileVersion.findUnique({
+      where: {
+        fileId_version: {
+          fileId: fileId!,
+          version: version,
+        },
+      },
+    });
+
+    if (!fileVersion?.id)
+      return res
+        .status(404)
+        .json({ message: "Requested file version doesn't exist." });
+
+    const s3Key = fileVersion?.s3Key;
+    const stream = await storage.get(s3Key);
+
+    res.setHeader("Content-Type", "application/octet-stream");
+    res.setHeader(
+      "Content-Disposition",
+      `inline; filename="${saved.savedFile?.originalname}"`,
+    );
+    stream.pipe(res);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteVersion = async (req: Request, res: Response) => {
+  const filename = req.params.filename;
+  const userId = (req as any).userId;
+  if (!filename || typeof filename !== "string")
+    return res.status(400).json({ message: "Incomplete input." });
+
+  try {
+    const saved = await findUserFile(userId, filename);
+    if (!saved.success)
+      return res.status(404).json({ message: "File doesn't exist." });
+
+    await prisma.$transaction(async (tx) => {
+      const deleted = await tx.fileVersion.delete({
+        where: {
+          id: saved.savedFile?.latestId!,
+        },
+      });
+
+      await tx.pendingDelete.create({
+        data: {
+          s3Key: deleted.s3Key,
+        },
+      });
+
+      const remainingVersions = await tx.file.findUnique({
+        where: { id: saved.savedFile?.id! },
+        include: { versions: true },
+      });
+
+      if (remainingVersions?.versions.length! > 0) {
+        // 4️⃣ Update latestId to newest remaining version
+        await tx.file.update({
+          where: { id: remainingVersions?.id! },
+          data: {
+            latestId: remainingVersions?.versions[0]?.id!,
+          },
+        });
+      } else {
+
+        await tx.file.delete({
+          where: { id: saved.savedFile?.id!},
+        });
+      }
+    });
+    res.status(200).json({message: "Deleted the latest version."})
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Internal server error" });
--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -2,7 +2,9 @@ import express from "express";
 import { verifyToken } from "../middleware/verifyToken";
 import {
   deleteFile,
+  deleteVersion,
   getFile,
+  getVersion,
   updateFile,
   uploadFile,
 } from "../controllers/file.controller";
@@ -15,3 +17,5 @@ fileRouter.post("/", verifyToken, rateLimiter,upload.single("file"), uploadFile)
 fileRouter.get("/:filename", verifyToken,rateLimiter, getFile);
 fileRouter.put("/:filename", verifyToken,rateLimiter, upload.single("file"), updateFile);
 fileRouter.delete("/allVersions/:filename", verifyToken,rateLimiter, deleteFile);
+fileRouter.get("/:filename/:version",verifyToken,rateLimiter,getVersion);
+fileRouter.delete("/latest/:filename",verifyToken,rateLimiter,deleteVersion);
